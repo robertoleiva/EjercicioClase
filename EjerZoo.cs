7c0ea71a41bbdc7c2d56f50f4cdf870496617470
robertoleiva <47877340+robertoleiva@users.noreply.github.com>
2019-02-28 22:58:47 -0600

    Add files via upload

---
+++ EjerZoo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejerciciozoologico
+{
+    class Program
+    {
+
+        public string grup1, grup2, grup3, grup4;
+
+        static void Main(string[] args)
+        {
+
+
+
+            Program zoo = new Program();
+            Console.WriteLine(zoo.gene1() + zoo.gene2() + zoo.gene3() + zoo.gene4());
+            Console.ReadKey();
+
+        }
+
+        string gene1()
+        {
+            return "Aves: 10 ";
+
+        }
+
+
+        string gene2()
+        {
+            return "Reptiles: 20 ";
+
+
+        }
+
+        string gene3()
+        {
+            return "Mamiferos: 15";
+
+
+        }
+
+        string gene4()
+        {
+            return "Acuaticos: 10 ";
+
+
+        }
+
+
+    }
+
+
+
+}
+
+
+    
+
